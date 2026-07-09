a6a13483d9d263028ff56dff8146381d1bae26c0
wanjiangzhi <wanjiangzhi@outlook.com>
2026-07-09 10:03:46 +0800

    添加作者与版本信息

---
+++ CWeiYanCPPLib/constants.hpp
@@ -0,0 +1,20 @@
+// Licensed under the GNU Lesser General Public License, Version 2.1
+
+//
+// Created by wanjiangzhi on 2026/7/9.
+//
+
+#ifndef WEIYAN_CONSTANTS_HPP
+#define WEIYAN_CONSTANTS_HPP
+
+namespace Weiyan {
+    namespace Constants {
+        constexpr const char* Authors[] {
+            "WJiangzhi"
+          };
+        constexpr auto VersionName = "1.0.0";
+        constexpr unsigned long Version = 100000;
+    }
+}
+
+#endif //WEIYAN_CONSTANTS_HPP
